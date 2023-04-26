c6ee08bc392ccc9ca0af62e125195fe11d4098dd
susahin80 <susahin80@users.noreply.github.com>
2023-04-26 15:31:04 +0300

    Update HelpBox.jsx

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -3,7 +3,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <p>{text}</p>
     </article>
   );
